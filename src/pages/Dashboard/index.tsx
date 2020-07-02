be1de7958361994504e958bfc46c2b4ef07ce093
Jonas Castro <jonascastro128@gmail.com>
2020-07-02 15:00:09 -0300

    styling dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="https://avatars2.githubusercontent.com/u/29816458?s=460&u=a21d950fad7bf1453d9b45d4ef13808c75d8cbf1&v=4">
+        <img src="" alt="Jonas" />
+        <div>
+          <strong>Title</strong>
+          <p>Description</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
